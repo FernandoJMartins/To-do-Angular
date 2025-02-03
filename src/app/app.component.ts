7c4a9dbb19b0dec7a07c81e11a8ab29a4905e306
Fer <julio8martins@gmail.com>
2025-02-03 15:55:55 -0300

    user login & listing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Usuario } from '../shared/model/usuario';
+import {MatCard} from '@angular/material/card';
+import {MatFormField} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {listaUsuarios} from '../shared/model/listaUsuarios';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  imports: [
+    MatCard,
+    MatFormField,
+    FormsModule,
+    NgForOf
+  ],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'projeto-p4s';
+  listaUsuarios: Usuario[] = listaUsuarios;
+  usuario: Usuario;
+
+  constructor() {
+    this.usuario = new Usuario();
+  }
+
+  cadastrar(): void {
+    this.listaUsuarios.push(this.usuario);
+    this.usuario = new Usuario();
+    console.log(this.listaUsuarios);
+  }
+
 }
---
+++ src/shared/model/listaUsuarios.ts
@@ -0,0 +1,6 @@
+import {Usuario} from './usuario';
+
+export const listaUsuarios : Usuario[] =[
+  new Usuario(1, 'Fulano', 'j@gmail.com', '123'),
+  new Usuario(2, 'Ciclano', 'c@gmail.com', '123')
+]
